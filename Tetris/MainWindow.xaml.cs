31cf1e56a6f9b065579eabfaef4481652f950787
Grimakar <christian.camacho@mail.de>
2021-05-03 23:07:25 +0200

    Add project files.

---
+++ Tetris/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tetris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string AppName = "Tetris";
+        const string Version = "0.0.0";
+        const string Developer = "Grimakar";
+        const string TimeOfDevelopment = "May 2021";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, $"{AppName}\n{Version}\n{TimeOfDevelopment} {Developer}.\nNo rights reserved...", $"About {AppName}");
+        }
+
+        private void MenuQuit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
